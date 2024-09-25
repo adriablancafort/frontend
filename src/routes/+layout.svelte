21edda47b7b68f2c9d822528793454af5d00add0
Adria Blancafort <adriablancafort@gmail.com>
2024-09-25 15:28:21 +0200

    fix: prerender for SPA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
     let { children } = $props();
-
-    export const ssr = false;
 </script>
 
 <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" />
