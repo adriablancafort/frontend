9b958977bf2aeb093c95e1941a17cb4d77cacec3
Adria Blancafort <adriablancafort@gmail.com>
2024-09-25 14:46:24 +0200

    feat: transformed into SPA with adaptor-static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
     let { children } = $props();
+
+    export const ssr = false;
 </script>
 
 <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" />
