ba610491a0cb3a7ebe08f0775f3b6cb88ae2e299
Adria Blancafort <adriablancafort@gmail.com>
2024-09-25 16:09:21 +0200

    feat: add tailwind css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
+    import "tailwindcss/tailwind.css";
+
     let { children } = $props();
 </script>
-
-<link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" />
 
 {@render children()}
