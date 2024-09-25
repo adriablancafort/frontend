a0e4a4d0e06d2d0f5b200eb2c77b19fb33ce9539
Adria Blancafort <adriablancafort@gmail.com>
2024-09-25 17:52:32 +0200

    feat: add demo components

---
+++ src/lib/components/Tabs.svelte
@@ -0,0 +1,5 @@
+<div role="tablist" class="tabs tabs-lifted">
+  <a role="tab" class="tab">Normal</a>
+  <a role="tab" class="tab tab-active">Normal</a>
+  <a role="tab" class="tab">Normal</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,58 @@
-<input type="checkbox" value="black" class="toggle theme-controller" />
+<script>
+    import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+    import Accordion from '$lib/components/Accordion.svelte';
+    import Stats from '$lib/components/Stats.svelte';
+    import Tabs from '$lib/components/Tabs.svelte';
+    import LoadingCard from '$lib/components/LoadingCard.svelte';
+    import Input from '$lib/components/Input.svelte';
+    import Search from '$lib/components/Search.svelte';
+</script>
+
+<ThemeSwitcher />
 
 <h1 class="text-3xl font-extrabold">Sonar App</h1>
 
-<button class="btn btn-primary">Primary</button>
+<div class="tooltip" data-tip="hello">
+  <button class="btn">Hover me</button>
+</div>
+
+<LoadingCard />
+
+<Search />
+
+<div class="form-control">
+  <label class="label cursor-pointer">
+    <span class="label-text">Remember me</span>
+    <input type="checkbox" checked="checked" class="checkbox" />
+  </label>
+</div>
+
+<Stats />
+
+<Tabs />
+
+<span class="loading loading-spinner loading-md"></span>
+
+<Accordion />
+
+<input type="range" min="0" max="100" value="40" class="range" />
+
+<div class="rating">
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input
+    type="radio"
+    name="rating-2"
+    class="mask mask-star-2 bg-orange-400"
+    checked="checked" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+</div>
+
+<select class="select select-bordered w-full max-w-xs">
+  <option disabled selected>Who shot first?</option>
+  <option>Han Solo</option>
+  <option>Greedo</option>
+</select>
+
+<Input />
