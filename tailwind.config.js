00d2b5c0e618e384d9fd80c5f99e00447755865a
Adria Blancafort <adriablancafort@gmail.com>
2024-09-25 16:24:18 +0200

    fix: load tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,svelte,js,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: ["lofi", "black"],
+    darkTheme: "black",
+    styled: true,
+    themeRoot: ":root",
   },
 };
 
